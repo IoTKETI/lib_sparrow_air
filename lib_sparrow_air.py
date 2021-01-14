92adb8753b007dcbbfaa1aa1e832287c97c69963
IoTKETI <iot.keti@gmail.com>
2021-01-14 17:17:10 +0900

    - edit code 2

--- lib_sparrow_air.py
+++ lib_sparrow_air.py
@@ -166,7 +166,7 @@ def missionPortData():
             # if ((not missionStr) or (missionStr[0] == b'\x00\n') or (len(missionStr) < 3)):
             if ((not missionStr) or (missionStr[0] == b'\x00\n')):
                 if (not missionStr):
-                    if (count < 10):
+                    if (count < 5):
                         count += 1
                         pass
                     else:
@@ -181,26 +181,6 @@ def missionPortData():
             else:
                 if (flag == 0):
                     flag = 1
-                    # arrAIRQ = missionStr[3].decode("utf-8").split(", ")
-                    # arrQValue = arrAIRQ[0].split(',')
-                    # airQ['PM25'] = float(arrQValue[0]) # (ug/m3)
-                    # airQ['PM10'] = float(arrQValue[1]) # (ug/m3)
-                    # airQ['CO'] = float(arrQValue[2]) # (ppb)
-                    # airQ['NO2'] = float(arrQValue[3]) # (ppb)
-                    # airQ['O3_org'] = float(arrQValue[4]) # (org/ppb)
-                    # airQ['O3_comp'] = float(arrQValue[5]) # (comp/ppb)
-                    # airQ['SO2_org'] = float(arrQValue[6]) # (org/ppb)
-                    # airQ['SO2_comp'] = float(arrQValue[7]) # (comp/ppb)
-                    # airQ['T'] = float(arrQValue[8]) # (C)
-                    # airQ['H'] = float(arrQValue[9]) # (%)
-                    # airQ['NO2_OP1'] = int(arrAIRQ[1]) # (mV)
-                    # airQ['NO2_OP2'] = int(arrAIRQ[2]) # (mV)
-                    # airQ['O3_OP1'] = int(arrAIRQ[3]) # (mV)
-                    # airQ['O3_OP2'] = int(arrAIRQ[4]) # (mV)
-                    # airQ['CO_OP1'] = int(arrAIRQ[5]) # (mV)
-                    # airQ['CO_OP2'] = int(arrAIRQ[6]) # (mV)
-                    # airQ['SO2_OP1'] = int(arrAIRQ[7]) # (mV)
-                    # airQ['SO2_OP2'] = int(arrAIRQ[8]) # (mV)
                     arrAIRQ = missionStr[3].decode("utf-8").replace(" ", "")
                     arrQValue = arrAIRQ.split(',')
                     airQ['PM25'] = float(arrQValue[0])  # (ug/m3)
@@ -226,26 +206,7 @@ def missionPortData():
                     send_data_to_msw(airQ)
                     airQ = json.loads(airQ)
                 else:
-                    # arrAIRQ = missionStr[0].decode("utf-8").split(", ")
-                    # arrQValue = arrAIRQ[0].split(",")
-                    # airQ['PM25'] = float(arrQValue[0]) # (ug/m3)
-                    # airQ['PM10'] = float(arrQValue[1]) # (ug/m3)
-                    # airQ['CO'] = float(arrQValue[2]) # (ppb)
-                    # airQ['NO2'] = float(arrQValue[3]) # (ppb)
-                    # airQ['O3_org'] = float(arrQValue[4]) # (org/ppb)
-                    # airQ['O3_comp'] = float(arrQValue[5]) # (comp/ppb)
-                    # airQ['SO2_org'] = float(arrQValue[6]) # (org/ppb)
-                    # airQ['SO2_comp'] = float(arrQValue[7]) # (comp/ppb)
-                    # airQ['T'] = float(arrQValue[8]) # (C)
-                    # airQ['H'] = float(arrQValue[9]) # (%)
-                    # airQ['NO2_OP1'] = int(arrAIRQ[1]) # (mV)
-                    # airQ['NO2_OP2'] = int(arrAIRQ[2]) # (mV)
-                    # airQ['O3_OP1'] = int(arrAIRQ[3]) # (mV)
-                    # airQ['O3_OP2'] = int(arrAIRQ[4]) # (mV)
-                    # airQ['CO_OP1'] = int(arrAIRQ[5]) # (mV)
-                    # airQ['CO_OP2'] = int(arrAIRQ[6]) # (mV)
-                    # airQ['SO2_OP1'] = int(arrAIRQ[7]) # (mV)
-                    # airQ['SO2_OP2'] = int(arrAIRQ[8]) # (mV)
+
                     arrAIRQ = missionStr[0].decode("utf-8").replace(" ", "")
                     arrQValue = arrAIRQ.split(',')
                     airQ['PM25'] = float(arrQValue[0])  # (ug/m3)
